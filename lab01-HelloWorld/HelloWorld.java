87002299e9f1ec6bec14a70e827764099f8f30eb
Benjamin E. Pingrey <bep217@lehigh.edu>
2014-08-26 20:33:56 +0000

    Updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//Ben Pingrey
+//Hello World Java Program
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//   print out the statement with a box around it
+    
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("#                     #");
+    System.out.println("#  Hello World 2014!  #");
+    System.out.println("#                     #");
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("test");
+    
+    }
+}
