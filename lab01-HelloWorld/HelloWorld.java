bafb6d220b11dbc4d4c47f43eef761ca33b5654d
Benjamin E. Pingrey <bep217@lehigh.edu>
2014-08-27 15:57:12 +0000

    Updated tabbing, changed "test" to "testing"

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -4,19 +4,19 @@
 //  define a class
 public class HelloWorld{
     
-//  add main method
+    //  add main method
     public static void main(String[] args) {
         
-//   print out the statement with a box around it
+        //   print out the statement with a box around it
     
-    System.out.println("#######################");
-    System.out.println("#######################");
-    System.out.println("#                     #");
-    System.out.println("#  Hello World 2014!  #");
-    System.out.println("#                     #");
-    System.out.println("#######################");
-    System.out.println("#######################");
-    System.out.println("test");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#  Hello World 2014!  #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("testing");
     
     }
 }
