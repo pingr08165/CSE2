b3a2ca82431272e63c3b9a1b8799a5bac8c8f770
Benjamin E. Pingrey <bep217@lehigh.edu>
2014-08-31 01:08:51 +0000

    Completed Homework 1 (hw01)

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+// Benjamin Pingrey
+// CSE2 Section 110
+// Homework 1--WelcomeClass
+
+// Program to print out a block of text
+
+// Create class
+public class WelcomeClass {
+    // Create main method
+    public static void main(String[] args) {
+        // Print out the required pattern and information
+        System.out.println("    -----------     ");
+        System.out.println("    | WELCOME |     ");
+        System.out.println("    -----------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        // Use "\\" to allow "\" to be printed
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-B--E--P--2--1--7-|"      );
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  "      );
+        System.out.println("I live in Oregon, so I'm a bit far from home here at Lehigh.");
+        System.out.println("I like Pennsylvania, but I wish it was cooler / less humid. ");
+    }
+}
